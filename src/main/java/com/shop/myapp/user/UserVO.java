0ba5ca43fd758dfe376f1ff855c5f29e13f604eb
김준우 <nmnmko04@gmail.com>
2020-12-10 21:54:20 +0900

    [UPDATE] Controller & Mybatis

---
+++ src/main/java/com/shop/myapp/user/UserVO.java
@@ -0,0 +1,47 @@
+package com.shop.myapp.user;
+
+public class UserVO {
+	private String uid;
+	private String username;
+	private String password;
+	private String profileimg;
+	private String comment;
+	private String phoneNumber;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getProfileimg() {
+		return profileimg;
+	}
+	public void setProfileimg(String profileimg) {
+		this.profileimg = profileimg;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
